e1f74aa8467df92d8e1f41a35573b48cbefec25b
Saki Aihara <sakiazaele@gmail.com>
2025-03-07 14:29:15 +0700

    pemweb2

---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+//Mendefinisikan Variabel
+$nama = "Sina";
+$umur = 15;
+$bb = 40;
+
+echo "Nama saya adalah : " . $nama;
+echo "<br/>Berat Badan : " . $bb . " Kg";
+echo "<br/>Umur : " . $umur . " Tahun";
+?>
